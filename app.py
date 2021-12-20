7fa76b9b4feb2ec6fc9de055171cbeddc6a0361f
Barry <barry@Barrys-MacBook-Pro.local>
2021-12-19 21:20:42 -0500

    set2

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask
-from surveys import surveys
+from flask import Flask, request, render_template, redirect, flash,
+from flask_debugtoolbar import DebugToolbarExtension
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "topsecretkey"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
